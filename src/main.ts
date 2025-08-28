c5a380126d303c498f7988b414c5eddb5fc98ede
Edward Mizuka <mizuka.wu@outlook.com>
2025-08-29 00:59:09 +0800

    修复: 修复 mxgraph 初始化时的配置错误

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as Y from "yjs";
 import { WebrtcProvider } from "y-webrtc";
-import { bindDrawioFile, xml2doc } from "./yjs";
+import { bindDrawioFile } from "./yjs";
 
 /**
  * 通过注入的方式拿到实例, 这个之后再考虑什么时候搞进去，3s后开发版是一定加载完成的
