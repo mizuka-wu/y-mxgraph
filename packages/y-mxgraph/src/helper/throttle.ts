1f5b53a4ba934a5ad2b4634813fa20946cefe14e
Edward Mizuka <mizuka.wu@outlook.com>
2026-05-18 07:00:39 +0000

    feat: 修复类型定义

--- packages/y-mxgraph/src/helper/throttle.ts
+++ packages/y-mxgraph/src/helper/throttle.ts
@@ -1,4 +1,4 @@
-export function throttle<T extends (...args: unknown[]) => unknown>(
+export function throttle<T extends (...args: any[]) => any>(
   fn: T,
   wait: number,
 ): (...args: Parameters<T>) => ReturnType<T> | undefined {
